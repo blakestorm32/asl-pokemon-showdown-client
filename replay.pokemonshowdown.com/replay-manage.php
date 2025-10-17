12224f7a549fc0c5fbd98a1837351f61167fbe59
Blake Peery <45606916+blakestorm32@users.noreply.github.com>
2025-10-16 21:53:37 -0600

    Update replay-manage.php

--- replay.pokemonshowdown.com/replay-manage.php
+++ replay.pokemonshowdown.com/replay-manage.php
@@ -3,6 +3,10 @@
 error_reporting(E_ALL);
 ini_set('display_errors', TRUE);
 ini_set('display_startup_errors', TRUE);
+ini_set('display_errors', '1');           // show on page (for now)
+ini_set('display_startup_errors', '1');
+ini_set('log_errors', '1');
+ini_set('error_log', 'php://stderr');     // send to Railway logs
 
 require_once __DIR__ . '/../config/config.inc.php';
 require_once __DIR__ . '/theme/panels.lib.php';
