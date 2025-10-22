01af3a0782814aac264b4e0a2e4ee851ed95dc8b
Blake Peery <blakeepeery@gmail.com>
2025-10-21 20:55:28 -0600

    test

--- config/config-example.js
+++ config/config-example.js
@@ -4,9 +4,9 @@ var Config = Config || {};
 /* version */ Config.version = "0";
 
 Config.defaultserver = {
-  id: 'asl',                                  // any id you like
-  host: 'https://aslpokemonbattling-up-railway-app.psim.us/',         // <-- change this
-  port: 443,                                  // HTTPS/WSS
+  id: 'asl', // any short lowercase id is fine
+  host: 'aslpokemonbattling-up-railway-app.psim.us', // 👈 no protocol or trailing slash
+  port: 443, // HTTPS/WSS
   altport: 80,
   registered: true
 };
